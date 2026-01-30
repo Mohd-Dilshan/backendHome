d0a378eb8c4c7a6ef91a1f7180e1d3cb150fc212
Mohd-Dilshan <mohddilshaninvertian@gmail.com>
2026-01-30 17:26:45 +0530

    Fix server routes and data display

--- index.js
+++ index.js
@@ -17,23 +17,52 @@
 const http = require('http');
 const url = require('url');  
 
+const fs = require('fs')
+
+const path = require('path')
+
+const filepath=path.join(__dirname + '/data.txt');
+
+const data = {
+    name : "Tushar Kumar",
+    aka : "The CEO",
+    age : 79,
+    college : "Invertis University,UP"
+}
+
+
+if (!fs.existsSync(filepath)) {
+    fs.writeFileSync(filepath, JSON.stringify([], null, 2));
+}
+
 const server = http.createServer((req, res) => {
     const parseUrl = url.parse(req.url, true);
     const pathName = parseUrl.pathname;
     const query = parseUrl.query;
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     if (pathName === '/') {
         res.write('<h1>Welcome to the Home Page</h1>');
         res.end();
-    }else if (pathName === '/addName') {
+    }
+    else if (pathName === '/addName') {
         const { name } = query;
-        res.write(`<H1>Hello ${name}`);
+
+        res.write(`<H1>Hello ${name}</H1>`);
+        res.end();
+    }
+    else if (pathName === '/data') {
+        const fileData = fs.readFileSync(filepath, 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(fileData);
+        res.end();
+    }else {
+        res.write('<h1>404 Page Not Found</h1>');
+        res.end();
     }
 });
 
-// ✅ start the server
+// start the server
 server.listen(3000, () => {
     console.log('http://localhost:3000');
 });
